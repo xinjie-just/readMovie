4f449f510d08ea26ce731192bf584148682563e9
xinjie-just <18227752005@qq.com>
2019-01-21 18:50:22 +0800

    add read-details
    添加阅读详情

--- pages/read/read.js
+++ pages/read/read.js
@@ -1,1 +1,27 @@
-Page({})
+var readData = require("../../data/read-data.js");
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      data: readData.readData
+    })
+  },
+
+  view(value) {
+    var readId = value.currentTarget.dataset.readid;
+    wx.navigateTo({
+      url: "read-details/read-details?id=" + readId,
+    })
+  }
+})
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,1 +1,72 @@
-Page({})
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  startMiniProgram: function(event) {
+    wx.redirectTo({ // 页面重定向到指定页面
+      url: "/pages/read/read",
+    })
+    // console.log("你点击了父元素");
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // console.log("welcome onLoad");
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    // console.log("welcome onReady");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    // console.log("welcome onShow");
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+    // console.log("welcome onHide");    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+    // console.log("welcome onUnload");    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
